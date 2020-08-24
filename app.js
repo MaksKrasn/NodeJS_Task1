83b67bcf33e50b808e20b1637feb24593efc7c57
MaksKrasn <uw5ekr@gmail.com>
2020-08-24 17:59:34 +0300

    add add.html and index.html

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ const settings = require('./settings');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, resp) => {
-    
-    console.log('Hello, Maks!!!')
+app.use(express.urlencoded({extended: false}));
+
+app.get('/home', (req, resp) => {
+    var now = new Date();
+    resp.sendFile(__dirname + '/index.html');
+    console.log('Hello, Maks!!!');
+
 });
 
-app.listen(settings.port, settings.host); 
+app.get('/data', (req, resp) => {
+
+});
+
+app.get('/add', (req, resp) => {
+    resp.sendFile(__dirname + '/add.html');
+});
+
+app.post('/add', (req, resp) => {
+    console.log(req.body);
+    resp.send('Success!!');
+});
+
+app.listen(settings.port, settings.host, () => {
+    console.log('Success start!!!');
+}); 
